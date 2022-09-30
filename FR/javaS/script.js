500e38e9a36c8809a7f0948435e203b4650e3d35
Рахим <114479886+Rahim-Kadyrov@users.noreply.github.com>
2022-10-01 01:01:51 +0300

    Add files via upload

---
+++ FR/javaS/script.js
@@ -0,0 +1,33 @@
+// Правильные переменные!
+alert('Привет')
+
+let $ = 7
+let r = 6
+r= $ + $
+$= r + $
+// Неправильные переменные! let 1a; let 75a;
+let g = 25;
+let b = 5;
+b = b + g;
+b + $;
+console.log(b + $);
+let s = 100;
+let h = 100;
+console.log(s === h);
+
+let name = prompt("Как зовут?");
+let age = prompt("Сколько лет?");
+
+    console.log(` Меня зовут  ${name}   Мне   ${age}  лет ` );
+
+
+    const k = 100;
+    let l = 10;
+    if(k > l){
+        console.log("константа больше 10");
+    }
+    else{
+        console.log("константа меньше 10");
+    }
+    
+    
